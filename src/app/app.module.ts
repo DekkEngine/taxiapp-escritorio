90da94bf4290ec60e4c5b1ee894c0321898dd2cd
Dekk <dekkservidores@gmail.com>
2018-06-25 01:13:55 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCLb7GTSMUAT8jw_fcnXZsPyObJ-a9WxLU',
+    }),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
-  providers: [],
+  providers: [
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBrIN0YemuW1fXJZ6jogfAFrhD_0-tFO0k',
+    authDomain: 'notificacion-def47.firebaseapp.com',
+    databaseURL: 'https://notificacion-def47.firebaseio.com',
+    projectId: 'notificacion-def47',
+    storageBucket: 'notificacion-def47.appspot.com',
+    messagingSenderId: '202735568551'
+  }
 };
 
 /*
